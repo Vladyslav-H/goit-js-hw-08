2d0fa334bf95804a3f4e60500a6591b7028ec15f
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-23 15:33:05 +0200

    update mod-08 task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,7 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import * as lightBox from '../../../goit-js-hw-07/js/02-lightbox'
+console.log(lightBox);
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
