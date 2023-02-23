07308b403eaf0490e646d093ab0d30b0b612ca1a
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-23 15:39:58 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,11 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import * as lightBox from '../../../goit-js-hw-07/js/02-lightbox'
-console.log(lightBox);
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import * as lightBox from '../../../goit-js-hw-07/js/02-lightbox'
+console.log(lightBox);
