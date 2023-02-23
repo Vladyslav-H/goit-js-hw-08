9d554a5ea9509bc09bc4cc51f5df981ab134e666
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-23 15:49:46 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,5 +7,26 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-import * as lightBox from '../../../goit-js-hw-07/js/02-lightbox'
-console.log(lightBox);
+const galleryEl = document.querySelector(".gallery");
+const galleryCards = createGalleryMarkup(galleryItems); const lightboxSettings = {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+};
+galleryEl.insertAdjacentHTML("afterbegin", galleryCards);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({
+        original,
+        preview,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", lightboxSettings);
