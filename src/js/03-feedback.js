6f6cb5d622920ff0aa260b42f9901345b623db27
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-25 10:02:15 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const savedInputEvent = localStorage.getItem('feedback-form-state');
+const parseInputEvent = JSON.parse(savedInputEvent);
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+populateForm();
+
+function onFormInput(event) {
+  const inputEvent = {
+    email: formEl.email.value,
+    message: formEl.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputEvent));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+}
+
+function populateForm() {
+  if (savedInputEvent) {
+    formEl.email.value = parseInputEvent.email;
+    formEl.message.value = parseInputEvent.message;
+  }
+}
