9d58e3a761fd6e8ad0019cbe334761537bcda263
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-02-25 12:52:06 +0200

    Fixed console message 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,9 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-const savedInputEvent = localStorage.getItem('feedback-form-state');
-const parseInputEvent = JSON.parse(savedInputEvent);
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const savedInputData = localStorage.getItem(LOCALSTORAGE_KEY);
+const parseInputData = JSON.parse(savedInputData);
 
 formEl.addEventListener('input', throttle(onFormInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
@@ -10,24 +11,23 @@ formEl.addEventListener('submit', onFormSubmit);
 populateForm();
 
 function onFormInput() {
-  const inputEvent = {
+  const inputData = {
     email: formEl.email.value,
     message: formEl.message.value,
   };
-  localStorage.setItem('feedback-form-state', JSON.stringify(inputEvent));
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputData));
 }
-console.log(parseInputEvent);
+
 function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
-  console.log(parseInputEvent);
-  localStorage.removeItem('feedback-form-state');
-  
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  localStorage.removeItem(LOCALSTORAGE_KEY);
 }
 
 function populateForm() {
-  if (savedInputEvent) {
-    formEl.email.value = parseInputEvent.email;
-    formEl.message.value = parseInputEvent.message;
+  if (savedInputData) {
+    formEl.email.value = parseInputData.email;
+    formEl.message.value = parseInputData.message;
   }
 }
